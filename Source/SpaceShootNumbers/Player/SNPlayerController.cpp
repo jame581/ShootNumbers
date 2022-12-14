7b6e468b2d1a08e6e74aeda28b7fd6b1c83d08e3
JaMe <jame_581@windowslive.com>
2022-12-14 22:01:55 +0100

    Player BP edited and extended

--- Source/SpaceShootNumbers/Player/SNPlayerController.cpp
+++ Source/SpaceShootNumbers/Player/SNPlayerController.cpp
@@ -6,6 +6,23 @@
 #include "SpaceShootNumbers/SNGameModeBase.h"
 #include <GameFramework/PlayerController.h>
 
+void ASNPlayerController::BeginPlay()
+{
+	//ASNGameModeBase* MyGameMode = Cast<ASNGameModeBase>(UGameplayStatics::GetGameMode(GetWorld()));
+	//if (IsValid(MyGameMode))
+	//{
+	//	MyGameMode->OnGameOverDelegate.AddDynamic(this, &ASNPlayerController::HandleGameOver);
+	//}
+}
+
+//void ASNPlayerController::HandleGameOver()
+//{
+//	//PlayerDeath();
+//
+//	//PlayerScore = InPlayerScore;
+//	//PlayTimeInSecods = InPlayTimeInSecods;
+//}
+
 void ASNPlayerController::PlayerDeath()
 {
 	ASNGameModeBase* MyGameMode = Cast<ASNGameModeBase>(UGameplayStatics::GetGameMode(GetWorld()));
@@ -14,4 +31,20 @@ void ASNPlayerController::PlayerDeath()
 		MyGameMode->GameOver();
 		DisableInput(this);
 	}
+}
+
+int32 ASNPlayerController::GetPlayerScore() const
+{
+	return PlayerScore;
+}
+
+int32 ASNPlayerController::GetPlayTimeInSecods() const
+{
+	return PlayTimeInSecods;
+}
+
+FString ASNPlayerController::GetPlayTimeInSecodsFormatted() const
+{
+	FTimespan CurrentPlayTime = FTimespan::FromSeconds(PlayTimeInSecods);
+	return FString::Printf(TEXT("%02d:%02d:%02d"), CurrentPlayTime.GetHours(), CurrentPlayTime.GetMinutes(), CurrentPlayTime.GetSeconds());
 }
--- Source/SpaceShootNumbers/Player/SNPlayerController.h
+++ Source/SpaceShootNumbers/Player/SNPlayerController.h
@@ -13,9 +13,32 @@ UCLASS()
 class SPACESHOOTNUMBERS_API ASNPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+protected:
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
 	
 public:
 
+	//UFUNCTION()
+	//virtual void HandleGameOver(int32 InPlayTimeInSecods, int32 InPlayerScore);
+
 	UFUNCTION()
 	virtual void PlayerDeath();
+
+	UFUNCTION(BlueprintCallable)
+	int32 GetPlayerScore() const;
+
+	UFUNCTION(BlueprintCallable)
+	int32 GetPlayTimeInSecods() const;
+
+	UFUNCTION(BlueprintCallable)
+	FString GetPlayTimeInSecodsFormatted() const;
+
+private: // Properties
+
+	int32 PlayerScore;
+
+	int32 PlayTimeInSecods;
 };
