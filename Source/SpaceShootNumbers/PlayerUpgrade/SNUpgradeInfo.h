49dcb6a493cbbd005e8a2d1aec3213b5b79f87c6
JaMe <jame_581@windowslive.com>
2023-07-11 20:28:26 +0200

    Damage upgrade added

--- Source/SpaceShootNumbers/PlayerUpgrade/SNUpgradeInfo.h
+++ Source/SpaceShootNumbers/PlayerUpgrade/SNUpgradeInfo.h
@@ -9,7 +9,8 @@
 UENUM()
 enum EUpgradeType
 {
-	FireRate    UMETA(DisplayName = "Fire Rate"),
+	FireRate			UMETA(DisplayName = "Fire Rate"),
+	DamageIncrease		UMETA(DisplayName = "Damage increase"),
 };
 
 
@@ -19,8 +20,12 @@ struct FSNUpgradeInfo
     GENERATED_BODY()
 	
 	// Amount of decrease player rate of fire
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Upgrade Info", meta = (ClampMin = "0.1", ClampMax = "2.0", UIMin = "0.1", UIMax = "2.0"))
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Upgrade Info", meta = (ClampMin = "0.1", ClampMax = "2.0", UIMin = "0.1", UIMax = "2.0", EditCondition = "UpgradeType == EUpgradeType::FireRate", EditConditionHides))
 	float RateOfFire;
+
+	// Amount of player damage
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Upgrade Info", meta = (ClampMin = "1", ClampMax = "5", UIMin = "1", UIMax = "5", EditCondition = "UpgradeType == EUpgradeType::DamageIncrease", EditConditionHides))
+	int32 PlayerDamage = 1;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Upgrade Info")
 	TEnumAsByte<EUpgradeType> UpgradeType;
